2afea098d2a7963ba9791246c7c503e8134dea8e
lmaoyouhyun <cadry238@gmail.com>
2023-12-19 16:21:54 +0900

    12/19

---
+++ chat.js
@@ -0,0 +1,7 @@
+function openPopup() {
+  document.getElementById('ch_overlay').style.display = 'flex';
+}
+
+function closePopup() {
+  document.getElementById('ch_overlay').style.display = 'none';
+}
